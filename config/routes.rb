3611f31f70e19877bdc4798cd89e1480cb5c5c56
Przemek Pelszynski <p.pelszynski@selleo.com>
2019-09-05 14:56:10 +0200

    Added user login and registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  devise_for :users,
+             path: '',
+             path_names: {
+               sign_in: 'login',
+               sign_out: 'logout',
+             },
+             controllers: {
+               sessions: 'sessions',
+             }
+
+  devise_scope :user do
+    post "/sign_up" => "registrations#create"
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
