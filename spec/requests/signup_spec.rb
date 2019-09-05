3b544a1170e52b8f9dac3c8af6b519d5beadc0b4
Przemek Pelszynski <p.pelszynski@selleo.com>
2019-09-05 14:56:10 +0200

    Added user login and registration

--- spec/requests/signup_spec.rb
+++ spec/requests/signup_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe 'POST /sign_up', type: :request do
-  context 'when user is unauthenticated' do
+  context 'when user is not authenticated' do
     it 'returns 200' do
       params = { user: { email: 'user@example.com', password: 'password' } }
 
       post '/sign_up', params: params
 
-      expect(response.status).to eq 200
+      expect(response).to have_http_status(200)
     end
 
     it 'returns a new user' do
@@ -15,13 +15,18 @@ RSpec.describe 'POST /sign_up', type: :request do
 
       post '/sign_up', params: params
 
-      expect(response.body).to match_schema('user')
+      parsed_response = JSON.parse(response.body)
+
+      expect(parsed_response).to include(
+        'id' => be_kind_of(Integer),
+        'email' => 'user@example.com'
+      )
     end
   end
 
   context 'when user already exists' do
     it 'returns bad request status' do
-      User.create!(email: 'user@example.com')
+      create(:user, email: 'user@example.com', password: 'password')
       params = { user: { email: 'user@example.com', password: 'password' } }
 
       post '/sign_up', params: params
@@ -30,7 +35,7 @@ RSpec.describe 'POST /sign_up', type: :request do
     end
 
     it 'returns validation errors' do
-      User.create!(email: 'user@example.com')
+      create(:user, email: 'user@example.com', password: 'password')
       params = { user: { email: 'user@example.com', password: 'password' } }
 
       post '/sign_up', params: params
